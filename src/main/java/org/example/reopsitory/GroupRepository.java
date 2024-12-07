af833202c4f3d4b7000a181299066833c223167a
Szuum02 <s.urbanski2002@gmail.com>
2024-12-07 16:56:16 +0100

    group repository - tests

--- src/main/java/org/example/reopsitory/GroupRepository.java
+++ src/main/java/org/example/reopsitory/GroupRepository.java
@@ -11,18 +11,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
-    @Query("select new org.example.dtos.groups.GroupDTO(g.id, g.subject) from Group g " +
-            "where ?1 member of g.students order by g.subject")
-    List<GroupDTO> getGroupsByStudent(Student student);
-
-    @Query("select s.id, s.name, s.lastname, s.index from Group g inner join g.students s where g.id = ?1" +
-            " order by s.lastname")
-    public List<Object[]> getStudentsByGroup(Long groupId);
-
     public Group findById(long id);
-
-    @Query("select l.id, l.topic from Group g inner join g.lessons l where g.id = ?1 order by l.date")
-    public List<Object[]> getLessons(long groupId);
 
     @Query("select new org.example.dtos.groups.ReportDTO(g.id, g.subject)" +
             " from Group g where g.id = ?1")
